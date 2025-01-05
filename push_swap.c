4732632f6dcebf410a5feaa37d0ad59fb771b32d
zuccc23 <daniyaahmane@gmail.com>
2025-01-05 18:11:49 +0100

    jsp

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dahmane <dahmane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 16:19:03 by dahmane           #+#    #+#             */
-/*   Updated: 2025/01/03 17:26:18 by dahmane          ###   ########.fr       */
+/*   Updated: 2025/01/05 17:18:16 by dahmane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,26 @@ int	valid_param(char **strs)
 	return (0);
 }
 
-
+void getBit(int num, int i)
+{
+	if ((num & (1 << i)) != 0)
+		printf("1");
+	else 
+		printf("0");
+}
+void printbin(int n)
+{
+	if (n > 1)
+		printbin(n / 2);
+	printf("%d", n % 2);
+}
 int main(int argc, char **argv)
 {
 	// if (argc < 2)
 	// 	return (0);
 	// printf("%d", valid_param(argv));
-	printf("%d\n", 7);
+	// getBit(5, 10);
+	// printbin(5);
+	int	tab[] = {2, 3, 5, 4};
+	
 }
