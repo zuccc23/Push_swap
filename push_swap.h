70475752d23d0519de319621653058188f377e9a
zuccc23 <daniyaahmane@gmail.com>
2025-01-05 19:11:03 +0100

    owowowowoow

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dahmane <dahmane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 16:39:07 by dahmane           #+#    #+#             */
-/*   Updated: 2025/01/03 17:07:17 by dahmane          ###   ########.fr       */
+/*   Updated: 2025/01/05 18:45:41 by dahmane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdarg.h>
+
+typedef struct t_list s_list;
+struct t_list
+{
+	int	data;
+	struct t_list *next;
+};
 
 int	ft_isdigit(int c);
 int	valid_char(const char *s);
