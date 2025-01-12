166346d83b28952622184025aabeac8150ed2d39
zuccc23 <daniyaahmane@gmail.com>
2025-01-12 20:46:06 +0100

    sort 3

--- push_swap.c
+++ push_swap.c
@@ -6,12 +6,31 @@
 /*   By: dahmane <dahmane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 16:19:03 by dahmane           #+#    #+#             */
-/*   Updated: 2025/01/12 15:25:18 by dahmane          ###   ########.fr       */
+/*   Updated: 2025/01/12 19:34:07 by dahmane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	sort_3(s_list **stack)
+{
+	s_list	*second;
+	s_list	*third;
+
+	second = (*stack)->next;
+	third = second->next;
+	if ((*stack)->data > second->data && (*stack)->data > third->data)
+		rotate(&(*stack));
+	if (second->data > (*stack)->data && second->data > third->data)
+		rotate_down(&(*stack));
+	second = (*stack)->next;
+	
+	
+	// if (second->data > third->data)
+	// {
+	// 	rotate_down(&(*stack));
+	// }
+}
 
 int main(int argc, char **argv)
 {
@@ -21,17 +40,17 @@ int main(int argc, char **argv)
 	// getBit(5, 10);
 	// printbin(5);
 	// bin(7);
-	int	tab[] = {2, 13, 3, 7};
+	int	tab[] = {3, 1, 2};
 	int	tab2[] = {8, 29, 4, 12};
 	s_list	*stack_a = NULL;
 	s_list	*stack_b = NULL;
 	s_list	*test = NULL;
-	stack_a = assign_list(tab, 4);
+	stack_a = assign_list(tab, 3);
 	stack_b = assign_list(tab2, 4);
 	
-	
-	// print_list(stack_a);
+	// sort_3(&stack_a);
+	rotate_down(&stack_a);
+	print_list(stack_a);
 	// printf("\n");
 	// print_list(stack_b);
-
 }
